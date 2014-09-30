752343fad06d288b3750a83e73a067e00bb64830
zandao <alexandre.drummond@gmail.com>
2014-09-30 10:41:14 -0300

    Retorno de erro 404 quando pessoa não é encontrada

--- src/main/java/br/jus/trt3/poc/jee6/rest/application/PessoaRS.java
+++ src/main/java/br/jus/trt3/poc/jee6/rest/application/PessoaRS.java
@@ -4,10 +4,16 @@ import br.jus.trt3.poc.jee6.entity.Pessoa;
 import br.jus.trt3.poc.jee6.repository.PessoaRepository;
 import java.util.List;
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import org.jboss.resteasy.spi.HttpResponse;
+import org.jboss.resteasy.spi.NotFoundException;
 
 /**
  *
@@ -21,9 +27,31 @@ public class PessoaRS {
 
     @GET
     @Produces("application/json")
-    public List<Pessoa> getPessoas(@QueryParam("nome") String nome, @QueryParam("telefone") String telefone) {
+    public List<Pessoa> getPessoas(@QueryParam("nome") String nome, 
+            @QueryParam("telefone") String telefone, @Context final HttpServletResponse response)
+            throws NotFoundException {
         List<Pessoa> pessoas = pessoaRepository.findByNomeETelefone(nome, telefone);
-
+        
+        if (pessoas.isEmpty()) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            try {
+                response.flushBuffer();
+            } catch(Exception e){}
+        }
         return pessoas;
+    }
+    
+    @GET
+    @Produces("application/json")
+    @Path("/id/{id}")
+    public Pessoa getPessoa(@PathParam("id") Long id, @Context final HttpServletResponse response) throws NotFoundException{
+        Pessoa pessoa = pessoaRepository.findBy(id);
+        if (pessoa == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            try {
+                response.flushBuffer();
+            } catch(Exception e){}
+        }
+        return pessoa;
     }
 }
