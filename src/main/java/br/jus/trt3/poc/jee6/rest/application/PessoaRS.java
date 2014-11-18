0c03392a8c6864bbd06c3774dcba914c2748e47f
zandao <alexandre.drummond@gmail.com>
2014-11-18 13:48:28 -0200

    Logging: Inclusão na classe PessoaRS de exemplo de uso de LogMarker.

--- src/main/java/br/jus/trt3/poc/jee6/rest/application/PessoaRS.java
+++ src/main/java/br/jus/trt3/poc/jee6/rest/application/PessoaRS.java
@@ -3,8 +3,8 @@ package br.jus.trt3.poc.jee6.rest.application;
 import br.jus.trt3.poc.jee6.entity.Pessoa;
 import br.jus.trt3.poc.jee6.entity.Telefone;
 import br.jus.trt3.poc.jee6.repository.PessoaRepository;
+import br.jus.trt3.poc.jee6.rest.application.config.LogMarker;
 import java.util.List;
-import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.ws.rs.Consumes;
@@ -48,59 +48,63 @@ public class PessoaRS {
     @GET
     public List<Pessoa> getPessoas(@QueryParam("nome") String nome, 
             @QueryParam("telefone") String telefone) {
+        logger.entry(nome, telefone);
         List<Pessoa> pessoas = pessoaRepository.findByNomeETelefone(nome, telefone);
         
         if (pessoas.isEmpty()) {
-            logger.debug("Pessoa com nome '{}' não encontrada", nome);
+            logger.debug(LogMarker.DB_QUERY, "Pessoa com nome '{}' não encontrada", nome);
             ResponseBuilder builder = Response.status(Status.NOT_FOUND);
             throw new WebApplicationException(builder.build());
         }
-        logger.debug("{} pessoas encontradas", pessoas.size());
-        return pessoas;
+        logger.debug(LogMarker.DB_QUERY, "{} pessoas encontradas", pessoas.size());
+        return logger.exit(pessoas);
     }
     
     @GET
     @Path("/{id: \\d+}")
     public Pessoa getPessoa(@PathParam("id") Long id){
+        logger.entry(id);
         Pessoa pessoa = pessoaRepository.findBy(id);
         if (pessoa == null) {
             ResponseBuilder builder = Response.status(Status.NOT_FOUND);
             builder.entity("{ \"error\": \"Entidade não encontrada\" }");
-            throw new WebApplicationException(builder.build());
+            throw logger.throwing(new WebApplicationException(builder.build()));
         }
-        return pessoa;
+        return logger.exit(pessoa);
     }
     
     @POST
     public Response add(Pessoa pessoa) {
+        logger.entry(pessoa);
         for (Telefone telefone : pessoa.getTelefones()) {
             telefone.setPessoa(pessoa);
         }
         pessoaRepository.save(pessoa);
-        return Response.status(Status.CREATED).entity(pessoa.getId()).build();
+        return logger.exit(Response.status(Status.CREATED).entity(pessoa.getId()).build());
     }
    
     @PUT
     public Pessoa update(Pessoa pessoa) {
+        logger.entry(pessoa);
         Pessoa pessoaParaAtualizar = pessoaRepository.findBy(pessoa.getId());
         if (pessoaParaAtualizar != null) {
             pessoaRepository.save(pessoa);
-            return pessoa;
+            return logger.exit(pessoa);
         } else {
-            throw new WebApplicationException(Response.status(Status.NOT_FOUND).build());
+            throw logger.throwing(new WebApplicationException(Response.status(Status.NOT_FOUND).build()));
         }
     }
     
     @DELETE
     @Path("/{id: \\d+}")
     public Response delete(@PathParam("id") Long id) {
+        logger.entry(id);
         Pessoa pessoa = pessoaRepository.findBy(id);
         if (pessoa != null) {
             entityManager.remove(entityManager.merge(pessoa));
-            return Response.status(Status.ACCEPTED).build();
+            return logger.exit(Response.status(Status.ACCEPTED).build());
         } else { 
-            return Response.status(Status.NOT_FOUND).build();
+            return logger.exit(Response.status(Status.NOT_FOUND).build());
         }
-        
     }
 }
