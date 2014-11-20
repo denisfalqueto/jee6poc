b2caa8a63ffd5a8f33a453ea60883dc337673484
Denis Falqueto <denisfalqueto@gmail.com>
2014-11-20 14:12:30 -0200

    Adaptar o script de geração de senhas ao Wildfly 8.1.0

--- bin/cryptpassword.sh
+++ bin/cryptpassword.sh
@@ -10,8 +10,7 @@ if [[ ! $1 ]]; then
   exit -1
 fi
 
-java -cp $JBOSS_HOME/modules/system/layers/base/org/picketbox/main/picketbox-4.0.19.SP8-redhat-1.jar:\
-$JBOSS_HOME/modules/system/layers/base/org/picketbox/main/picketbox-4.0.19SP8-redhat-1.jar:\
+java -cp $JBOSS_HOME/modules/system/layers/base/org/picketbox/main/picketbox-4.0.21.Beta1.jar:\
 $JBOSS_HOME/modules/system/layers/base/org/jboss/logging/main/jboss-logging-3.1.4.GA-redhat-1.jar:\
-$JBOSS_HOME/modules/system/layers/base/org/picketbox/main/picketbox-commons-1.0.0.final-redhat-2.jar:\
+$JBOSS_HOME/modules/system/layers/base/org/picketbox/main/picketbox-commons-1.0.0.final.jar:\
 $CLASSPATH org.picketbox.datasource.security.SecureIdentityLoginModule $1
